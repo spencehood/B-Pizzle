7eaabd8acdda15c756f990e240c97df669b7146f
spencehood <spho6674@colorado.edu>
2016-05-01 16:19:24 -0600

    Adding all files

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+
+/*
+	$('something').mouseenter(function() {
+		// action when hovering
+	}).mouseleave(function() {
+		// action when hovering ceases
+	});
+*/
+
+/*
+	setTimeout(function(){
+		// action after 900ms
+	}, 900);
+*/
+
+// window.location = 'page I want to navigate to.html';
+
+/*
+	$(window).scroll(function() {
+		var scrollTop = $(window).scrollTop();
+		// do something based on scrollTop
+	});
+*/
+
+/*
+	$(window).load(function() {
+		// action to occur right when page loads
+	});
+*/
+
+/*
+	$.fn.scrollView = function () {
+	  return this.each(function () {
+	    $('html, body').animate({
+	      scrollTop: $(this).offset().top
+	    }, 1000); // will take 1sec to scroll here
+	  });
+	}
+
+	$('#element-I-want-to-scroll-to').scrollView();
+*/
+
+});
